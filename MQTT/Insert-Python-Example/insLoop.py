e5abb0d3f454d5cf2fb5dba73dbdccf31bc322cd
darint00 <darint00@gmail.com>
2017-11-21 22:13:36 -0600

    Additional insLoop changes

--- MQTT/Insert-Python-Example/insLoop.py
+++ MQTT/Insert-Python-Example/insLoop.py
@@ -1,9 +1,7 @@
+from json import JSONEncoder
+from datetime import datetime
 import paho.mqtt.client as mqtt
 
-import json
-import pprint
-from datetime import datetime 
-from json import JSONEncoder
 
 NUMINS = 20000
 
@@ -15,37 +13,28 @@ def on_publish(client, userdata, mid):
         client.disconnect()
 
 
-
 client = mqtt.Client(protocol=mqtt.MQTTv31)
 client.on_publish = on_publish
 
-client.connect("127.0.0.1", 27883) # 10.0.68.69
+client.connect("127.0.0.1", 27883)
 
 client.loop_start()
 
-for i in range(1,NUMINS + 1):
-   ct = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-1];
-   msg = JSONEncoder().encode( {  
-      "sensor_id":"4",
-      "tstamp" : ct,
-      "d" : { "col4": "king bob"}  
-   } )
-   msgstr =  '{  "sensor_id":%d, "tstamp" : "%s",  "d" : { "col4": "king bob"}  }'  % (i,ct)
-   #msg = JSONEncoder().encode(msgstr)
+for i in range(1, NUMINS + 1):
+    ct = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-1]
+    msg = JSONEncoder().encode({
+        "sensor_id":"4",
+        "tstamp" : ct,
+        "d" : {"col4": "king bob"}
+        })
+    msgstr = '{  "sensor_id":%d, "tstamp" : "%s",  "d" : { "col4": "king bob"}  }'  % (i, ct)
 
-   (result,mid) = client.publish("wd.sensors_vti", msgstr,qos=0)
-   #print "Result, mid: ", result,mid
-   if ( result != mqtt.MQTT_ERR_SUCCESS) :
-         print ("Error Publish: ", i)
+    (result, mid) = client.publish("wd.sensors_vti", msgstr, qos=0)
+    if result != mqtt.MQTT_ERR_SUCCESS:
+        print("Error Publish: ", i)
 
- 
-   #client.loop(5.0, 1000) 
-   #client.publish("wd.sensors_vti_col", msg)
-   #print "JSON: " + json.dumps(msg,sort_keys=True,indent=4)
-
-   if ( (i % 1000) == 0 ):
-      print ("I: " , i) 
-      #time.sleep(.5)
+    if  (i % 1000) == 0:
+        print("I: ", i)
 
 
 client.loop_forever()
