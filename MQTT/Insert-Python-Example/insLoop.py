fdfdbac789038f89b67b300642c966c43de1786a
darint00 <darint00@gmail.com>
2017-11-22 09:33:17 -0600

    .

--- MQTT/Insert-Python-Example/insLoop.py
+++ MQTT/Insert-Python-Example/insLoop.py
@@ -13,12 +13,34 @@ def on_publish(client, userdata, mid):
         client.disconnect()
 
 
+ # def insert_collection():
+ #     """ insertCollection
+ #     """
+ #     for i in range(1, NUMINS + 1):
+ #         ct = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-1]
+ #         msg = JSONEncoder().encode({
+ #               "sensor_id":"4",
+ #               "tstamp" : ct,
+ #               "d" : {"col4": "king bob"}
+ #               })
+ #         msgstr = '{  "sensor_id":%d, "tstamp" : "%s",  "d" : { "col4": "king bob"}  }'  % (i, ct)
+#         (result, mid) = client.publish("wd.sensors_vti", msgstr, qos=0)
+#         if result != mqtt.MQTT_ERR_SUCCESS:
+#               print("Error Publish: ", i)
+#         if  (i % 1000) == 0:
+#             print("I: ", i)
+
+
+
+
 client = mqtt.Client(protocol=mqtt.MQTTv31)
 client.on_publish = on_publish
 
 client.connect("127.0.0.1", 27883)
 
 client.loop_start()
+
+insertCollection()
 
 for i in range(1, NUMINS + 1):
     ct = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-1]
