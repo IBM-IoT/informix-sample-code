e813966d12b0fcdf5f79c90b3aa4ffdcac301343
darint00 <darint00@gmail.com>
2017-11-29 13:30:37 -0600

    Adding to mqtt

--- MQTT/Insert-Python-Examples/insMqttTS.py
+++ MQTT/Insert-Python-Examples/insMqttTS.py
@@ -3,9 +3,9 @@
 from json import JSONEncoder
 from datetime import datetime
 import paho.mqtt.client as mqtt
+import time
 
-
-NUMINS = 20000
+NUMINS = 10
 
 def on_publish(client, userdata, mid):
     """ on_publish:
@@ -24,16 +24,23 @@ client.loop_start()
 
 
 for i in range(1, NUMINS + 1):
-    #ct = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-1]
-    ct = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    msg = JSONEncoder().encode({
-        "sensor_id":"4",
-        "tstamp" : ct,
-        "d" : {"col4": "king bob"}
-        })
-    msgstr = '{  "id":1, "desc":"description data",  "ts" : "%s",  "reading" : "king david" }'  % (ct)
-    msgstr = '{  "id":1, "ts" : "%s",  "reading" : "king david" }'  % (ct)
+    #currentTimeStr = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")[:-1]
 
+    currentTimeNow = time.time()
+    currentTimeInt = int(currentTimeNow*1000)
+    currentTimeStr = datetime.fromtimestamp(currentTimeNow).strftime("%Y-%m-%dT%H:%M:%S.%f")[:-1]
+
+    msgstr = '{  "id":1, "desc":"description data",  "ts" : {"$date":%s},  "reading" : {"col4": "king david" } }'  % (currentTimeInt)
+    msgstr = '{  "id":1, "desc":"description data",  "ts" : {"$date":%s},  "col1"  : "king bob", "col2":"King fred"  }'  % (currentTimeInt)
+
+    msgstr = '{  "id":1,  "ts" : {"$date":%s},  "col1"  : "king bob", "col2":"king fred"  }'  % (currentTimeInt)
+
+    #msgstr = '{  "id":1,  "ts" : "%s",  "col1"  : "king bob"  }'  % (currentTimeStr)
+
+    print("Publish ",msgstr)
+    print("   TimeNow:",currentTimeNow)
+    print("   TimeInt:",currentTimeInt)
+    print("   TimeStr:",currentTimeStr)
     (result, mid) = client.publish("iot.iot_data_ts", msgstr, qos=1)
     if result != mqtt.MQTT_ERR_SUCCESS:
         print("Error Publish: ", i)
